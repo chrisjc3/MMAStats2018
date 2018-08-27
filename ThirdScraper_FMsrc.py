dc3e34438bc67ddde226960afb16d5ac14dcd27a
ferret <intoxicatedferet@hotmail.com>
2018-08-27 18:54:16 -0400

    working out some bugs....

--- ThirdScraper_FMsrc.py
+++ ThirdScraper_FMsrc.py
@@ -10,6 +10,9 @@ from bs4 import BeautifulSoup
 import re
 import numpy as np
 from decimal import *
+import datetime
+import time
+from datetime import date, datetime, time, timedelta
 
 pd.set_option('display.max_colwidth', -1)
 
@@ -23,6 +26,12 @@ config = {
     'do_caching': False,
     'output_filename': ''
 }
+
+##############################################################################################################################
+##############################################################################################################################
+##########################      URL/HTML GATHERING          ##################################################################
+##############################################################################################################################
+##############################################################################################################################
 
 def readinJsonSearch(name):
     with open(name + '.json') as f:
@@ -83,8 +92,6 @@ def is_good_response(resp):
 def log_error(e):
     print(e)
 
-
-
 def getRDSsite(name, url):
     raw_html = simple_get(url)
     with open (name + '.html', 'a') as f:
@@ -111,101 +118,11 @@ def getHTML(name,url,force=False):
             soup = getRDSsite(name, url)
     return(soup)
 
-
-
-
-########################WEIGHTS
-
-
-
-
-def getReachWt(vit):
-    factor1 = float(vit['HeightCM'])*.02
-    ArmWt = ((float(vit['ReachCM']) - float(vit['HeightCM']))/factor1)*0.1
-    return(round(ArmWt,3))
-
-def getEndMethodWt(name,pf,howMany):
-    pf = pf[pf.Name.str.contains(name) == True]
-    pf = pf[pf.WinLoss.str.contains("NEXT") == False]
-    pf = pf[pf.WinLoss.str.contains("SD") == False]
-    npf = pf[0:howMany]
-    resultWt = 0
-    for i, v in npf.iterrows():
-        if str(v['WinLoss']) == "W":
-            try:
-                g = re.search(r'(KO.+|Submiss.+)',str(v['EndMethod']))
-                if g.group(1) != None:
-                    if int(v['Round']) == 1: resultWt= Decimal(resultWt) + Decimal(2.0)
-                    if int(v['Round']) == 2: resultWt= Decimal(resultWt) + Decimal(1.5)
-                    if int(v['Round']) == 3: resultWt= Decimal(resultWt) + Decimal(.5)
-                    if int(v['Round']) == 4: resultWt= Decimal(resultWt) + Decimal(.2)
-                    if int(v['Round']) == 5: resultWt= Decimal(resultWt) + Decimal(.1)
-            except:
-                resultWt= Decimal(resultWt) + Decimal(.1) 
-        else:
-            resultWt= Decimal(resultWt) - Decimal(.3)
-    return(round(resultWt,3))
-
-def getTendencyWts(name,pf,howMany):
-    pf = pf[pf.Name.str.contains(name) == True]
-    pf = pf[pf.WinLoss.str.contains("NEXT") == False]
-    npf = pf[0:howMany]
-
-    stkWt = Decimal(sum(npf['Strike']))*Decimal(0.005)
-    tdWt = Decimal(sum(npf['TakeDowns']))*Decimal(0.02)
-    subWt = Decimal(sum(npf['SubAtts']))*Decimal(0.0025)
-    passWt = Decimal(sum(npf['GPasses']))*Decimal(0.01)
-    TendencyWt = Decimal(stkWt)+Decimal(tdWt)+Decimal(subWt)+Decimal(passWt)
-    return(round(TendencyWt,3))
-    
-def defineWeights(name,vit,pf):
-    reachWt = getReachWt(vit)
-    EndMethodWt = getEndMethodWt(name, pf, 3)   #how_many previous fights to look at 
-    TendencyWt = getTendencyWts(name, pf, 3)   #how_many previous fights to look at 
-    Weight = Decimal(reachWt) + Decimal(EndMethodWt) + Decimal(TendencyWt)
-    return(round(Weight,3))
-
-
-
-
-
-
-
-
-
-
-
-###############SCARPER
-
-
-
-
-def getRDSVitalstats(name,soup):
-    data = pd.DataFrame()
-    i = 0
-    regex = re.compile('b\-box\_\_value')
-    for div in soup.findAll("i", {"class" : regex}):
-        try:
-             if i == 0: pass
-             if i == 1: data.loc[0, i] = div.contents[0].replace("  ","").replace("\\n","").replace("\\","")
-             if i == 2: data.loc[0, i] = div.contents[0].replace("  ","").replace("\\n","").replace("\\","")
-             if i == 3: data.loc[0, i] = int(div.contents[0].replace("  ","").replace("\\n","").replace("\\",""))
-             if i == 4: data.loc[0, i] = div.contents[0].replace("  ","").replace("\\n","").replace("\\","")
-             if i >= 5: pass
-             i+=1
-        except: pass
-    data = data.rename(index=str, columns={1: "Height",2: "Reach",3: "Age",4: "Stance"})
-
-    g = re.search(r'(\d)\'.*(\d+)\"',str(data['Height']))
-    n1 = int(g.group(1))
-    n2 = int(g.group(2))
-    data['HeightCM'] = (n1*30.48)+(n2*2.54)
-
-    g = re.search(r'(\d+)\"',str(data['Reach']))
-    n1 = int(g.group(1))
-    data['ReachCM'] = (n1*2.54)
-    data = data[['Height','HeightCM','Reach','ReachCM','Age','Stance']]
-    return data
+##############################################################################################################################
+##############################################################################################################################
+#################################   SCARPER         ##########################################################################
+##############################################################################################################################
+##############################################################################################################################
 
 def getRDSTablestats(name,soup):
 
@@ -239,7 +156,7 @@ def getRDSTablestats(name,soup):
     j = 0
     rowadd = 0
     for div in soup.findAll("p", {"class" : regex}):
-        if re.match(r'(\d+|KO.*|DEC.*|SUB.*|.*DEC.*|Overturned|DQ)',div.contents[0].replace("  ","").replace("\\n","")):
+        if re.match(r'(\d+|KO.*|DEC.*|SUB.*|.*DEC.*|Overturned|DQ|CNC)',div.contents[0].replace("  ","").replace("\\n","")):
             if j == 4 or j == 5 or j == 6 or j == 7:
                 data.loc[i+rowadd,j] = div.contents[0].replace("  ","").replace("\\n","")
                 data.loc[i+rowadd+1,j] = div.contents[0].replace("  ","").replace("\\n","")
@@ -254,41 +171,141 @@ def getRDSTablestats(name,soup):
                     j=0 
                     i+=2
 ##    print(data)
-    
     data = data[0:correctRange]
     data = data.rename(index=str, columns={0: "Strike",1: "TakeDowns",2: "SubAtts",3: "GPasses",4: "EndMethod",5: "Round"})
     data = data[['Name','Strike','TakeDowns','SubAtts','GPasses','WinLoss','Round','EndMethod']]
+    data[['Strike','TakeDowns','SubAtts','GPasses','Round']] = data[['Strike','TakeDowns','SubAtts','GPasses','Round']].apply(pd.to_numeric)
+    
     print(data)
     return(data)
 
+def getRDSVitalstats(name,soup):
+    data = pd.DataFrame()
+    i = 0
+    regex = re.compile('b\-list\_\_box\-list\-item b\-list\_\_box\-list\-item\_type\_block')
+    for div in soup.findAll("li", {"class" : regex}):
+        try:
+             if i == 0: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","") #height
+             if i == 1: pass #weight
+             if i == 2: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","") #reach
+             if i == 3: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","") #stance
+             if i == 4: pass #dob
+             if i == 5: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","")#strikes landed per min
+             if i == 6: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","")#strike accuracy
+             if i == 7: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","")#strikes absorbed per min
+             if i == 8: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","")#strike defense
+             if i == 9: pass #blank
+             if i == 10: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","") #TD average per 15min
+             if i == 11: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","") #TD accuracy
+             if i == 12: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","") #TD defense
+             if i == 13: data.loc[0, i] = div.contents[2].replace("  ","").replace("\\n","").replace("\\","") #subs attempted per 15min
+             i+=1
+        except: pass
+    
+    data = data.rename(index=str, columns={0: "Height",
+                                           2: "Reach",
+                                           3: "Stance",
+                                           5: "StrkLandpM",
+                                           6: "StrkAcc",
+                                           7: "StrkAbsorbpM",
+                                           8: "StrkDef",
+                                           10: "TDAttp15M",
+                                           11: "TDAcc",
+                                           12: "TDDef",
+                                           13: "SubsAttp15M"})
+    try:
+        g = re.search(r'(\d)\'.*(\d+)\"',str(data['Height']))
+        n1 = int(g.group(1))
+        n2 = int(g.group(2))
+        data['HeightCM'] = (n1*30.48)+(n2*2.54)
+    except: data['HeightCM'] = 0 
+    try:
+        g = re.search(r'(\d+)\"',str(data['Reach']))
+        n1 = int(g.group(1))
+        data['ReachCM'] = (n1*2.54)
+    except: data['ReachCM'] = 0
+    print(data)
 
+    return data
+
+
+##############################################################################################################################
+##############################################################################################################################
+##########################################  WEIGHTS SECTION     ##############################################################
+##############################################################################################################################
+##############################################################################################################################
+
+def getReachWt(vit):
+    if vit['ReachCM'][0] != 0:
+        factor1 = float(vit['HeightCM'])*.02
+        ArmWt = ((float(vit['ReachCM']) - float(vit['HeightCM']))/factor1)*0.1
+        return(round(ArmWt,3))
+    else: return(0)
+
+def getEndMethodWt(name,pf,howMany):
+    pf = pf[pf.Name.str.contains(name) == True]
+    pf = pf[pf.WinLoss.str.contains("next") == False]
+    pf = pf[pf.WinLoss.str.contains("nc") == False]
+    npf = pf[0:howMany]
+    resultWt = Decimal(0)
+    for i, v in npf.iterrows():
+        if str(v['WinLoss']) == "win":
+            try:
+                g = re.search(r'(KO.*|SUB.*)',str(v['EndMethod']))
+                if g.group(1) != None:
+                    if int(v['Round']) == 1: resultWt= Decimal(resultWt) + Decimal(2.0)
+                    if int(v['Round']) == 2: resultWt= Decimal(resultWt) + Decimal(1.5)
+                    if int(v['Round']) == 3: resultWt= Decimal(resultWt) + Decimal(.5)
+                    if int(v['Round']) == 4: resultWt= Decimal(resultWt) + Decimal(.2)
+                    if int(v['Round']) == 5: resultWt= Decimal(resultWt) + Decimal(.1)
+            except:
+                resultWt= Decimal(resultWt) + Decimal(.1) 
+        else:
+            resultWt= Decimal(resultWt) - Decimal(.3)
+    return(round(resultWt,3))
+
+def getTendencyWts(name,pf,howMany):
+    pf = pf[pf.Name.str.contains(name) == True]
+    pf = pf[pf.WinLoss.str.contains("next") == False]
+    npf = pf[0:howMany]
+
+    stkWt = Decimal(sum(npf['Strike']))*Decimal(0.005)
+    tdWt = Decimal(sum(npf['TakeDowns']))*Decimal(0.02)
+    subWt = Decimal(sum(npf['SubAtts']))*Decimal(0.0025)
+    passWt = Decimal(sum(npf['GPasses']))*Decimal(0.01)
+    TendencyWt = Decimal(stkWt)+Decimal(tdWt)+Decimal(subWt)+Decimal(passWt)
+    return(round(TendencyWt,3))
+    
+def defineWeights(name,vit,pf):
+    reachWt = getReachWt(vit)
+    print("Reach Wt: " + str(reachWt))
+    EndMethodWt = getEndMethodWt(name, pf, 3)   #how_many previous fights to look at
+    print("End Method Wt: " + str(EndMethodWt))
+    TendencyWt = getTendencyWts(name, pf, 3)   #how_many previous fights to look at
+    print("Tendency Wt: " + str(TendencyWt))
+    Weight = Decimal(reachWt) + Decimal(EndMethodWt) + Decimal(TendencyWt)
+    return(round(Weight,3))
 
 def getFighter(name,config):
     data = URLFetch(name, config, False)
     soup = getHTML(name,data['url'], False)
-
 ##    print(soup.prettify())
-    
+    vitaldata = getRDSVitalstats(name,soup)
     tabledata = getRDSTablestats(name,soup)
-##    vitaldata = getRDSVitalstats(name,soup)
-##    Weight = defineWeights(name,vitaldata,tabledata)
-##    return(Weight) 
+    Weight = defineWeights(name,vitaldata,tabledata)
+    print(str(Weight))
+    return(Weight) 
 
 
 ########################FOR INDIVIDUAL TESTING PURPOSES######################
 ##name = "Jon Jones"
 ##name = "Artem Lobov"
-name = "Cory Sandhagen"
+##name = "Cory Sandhagen"
+name = "Kalindra Faria "
 Weight = getFighter(name,config)
 ########################FOR INDIVIDUAL TESTING PURPOSES######################
 
 
-
-####well shit...RDS doesn't come up for Eryk Anders (http://fightmetric.rds.ca/fighter/2954)
-#might need to use regular fightmetric...
-
-##
-##
 ##data = pd.read_csv('DKSalaries.csv')
 ##data = data[['Name','ID','Salary','AvgPointsPerGame','Game Info','TeamAbbrev']]
 ##data = data.sort_values(by=['AvgPointsPerGame','Salary'], ascending=False)
@@ -312,32 +329,35 @@ Weight = getFighter(name,config)
 ##    data.loc[i,'Weight'] = Weight
 ##
 ##data = data[['DKID','Salary','Weight','Fighter','Opponent']]
-##
-##writer = pd.ExcelWriter(name + '_Report.xlsx')
+
+
+##dt = date.today()
+##dt = dt.strftime("%d%b%Y")
+##writer = pd.ExcelWriter('DKReport_' + dt + '.xlsx')
 ##data.to_excel(writer,sheet_name='Sheet1',startrow=0, startcol=0, index=False)
 ##writer.save()
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
